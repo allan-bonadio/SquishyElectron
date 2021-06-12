893a2b5637f48efaeb4e325671001e9f5af9a61f
Allan Bonadio <orgmgr@tactileint.org>
2021-06-12 09:09:45 -0700

    an include file for your .bashrc .  For my .bashrc .

---
+++ config/dotQq.sh
@@ -0,0 +1,10 @@
+
+export SQUISH_ROOT=`dirname $PWD`
+#export SQUISH_ROOT=/dvl/squishyElectron/SquishyElectron
+
+alias qq='cd $SQUISH_ROOT'
+alias qqq='cd $SQUISH_ROOT/quantumEngine'
+alias qqqb='cd $SQUISH_ROOT/quantumEngine/building'
+alias qqqt='cd $SQUISH_ROOT/quantumEngine/testing'
+alias qqp='cd $SQUISH_ROOT/public'
+
