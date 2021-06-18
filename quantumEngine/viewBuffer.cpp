05186943be9fa82b8461bdfe7eeb0670399e08e7
Allan Bonadio <bojnac@tactileint.org>
2021-06-18 08:49:47 -0700

    renamed rowNum to pointNum added fourth column, vertexSerial

--- quantumEngine/viewBuffer.cpp
+++ quantumEngine/viewBuffer.cpp
@@ -1,6 +1,6 @@
 #include "qSpace.h"
 
-// prep wave & potential  data for GL.
+// prep wave & potential  data for GL.  For rows of floats in a big Float32Array will be fed directly into gl.
 float *viewBuffer;
 
 float *getViewBuffer(void) {
@@ -16,23 +16,29 @@ float updateViewBuffer() {
 	int nPoints = theSpace->nPoints;
 	qReal highest = 0;
 
-	for (int rowNum = 0; rowNum < nPoints; rowNum++) {
-		float *twoRowPtr = viewBuffer + rowNum * 8;
-		qCx *wavePtr = theWave + rowNum;
-		qReal *potPtr = thePotential + rowNum;
+	for (int pointNum = 0; pointNum < nPoints; pointNum++) {
+		float *twoRowPtr = viewBuffer + pointNum * 8;
+		qCx *wavePtr = theWave + pointNum;
+		qReal *potPtr = thePotential + pointNum;
 		qReal re = wavePtr->re;
 		qReal im = wavePtr->im;
 
 		twoRowPtr[0] = twoRowPtr[4] = re;
 		twoRowPtr[1] = twoRowPtr[5] = im;
 		twoRowPtr[2] = twoRowPtr[6] = potPtr[0];
-		//twoRowPtr[3] = twoRowPtr[7] = ?
+		twoRowPtr[3] = pointNum * 2.;  // vertexSerial: at zero
+		twoRowPtr[7] = pointNum * 2. + 1.;  // at magnitude, top
 
 		// while we're here, collect the highest point
 		qReal height = re * re + im * im;
 		if (height > highest)
 			highest = height;
 	}
+
+	printf("viewBuffer.cpp, latest:\n");
+	for (int i = 0; i < nPoints*2; i++)
+		printf("%6.3f  %6.3f  %6.3f  %6.3f\n",
+			viewBuffer[i*4], viewBuffer[i*4+1], viewBuffer[i*4+2], viewBuffer[i*4+3]);
 
 	return highest;
 }
