06fdd2863ac6461da9970c6483cc2cf851f15295
Allan Bonadio <orgmgr@tactileint.org>
2021-06-20 12:21:56 -0700

    split SquishPanel.js off from App

---
+++ src/SquishPanel.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ControlPanel from './ControlPanel';
+
+import {createStateNWave} from './wave/theWave';
+import {qeSpace, qeStartPromise, qeDefineAccess} from './wave/qEngine';
+import qe from './wave/qe';
+
+import SquishView from './views/SquishView';
+import abstractViewDef from './views/abstractViewDef';
+import flatViewDef from './views/flatViewDef';
+
+
+const DEFAULT_RESOLUTION = 5;
+const DEFAULT_CONTINUUM = qeSpace.contCIRCULAR;
+
+
+export const listOfViewClasses = {
+	flatViewDef,  // the original one
+	abstractViewDef,  // primitive dummy, also superclass of all these others
+};
+
+
+class SquishPanel extends React.Component {
+	constructor(props) {
+		super(props);
+
+		debugger;
+		this.state = {
+			// THE N and continuum for THE space we're currently doing
+			N: DEFAULT_RESOLUTION,
+			continuum: DEFAULT_CONTINUUM,
+
+			currentQESpace: null,
+			currentView: null,
+		};
+
+		this.canvas = null;
+
+		console.log(`SquishPanel constructor done`);
+	}
+
+	// the canvas per panel, one panel per canvas
+	setGLCanvas(canvas) {
+		debugger;
+		this.canvas = canvas;
+		canvas.squishPanel = this;
+	}
+
+	setNew1DResolution(N, continuum) {
+		debugger;
+		qe.theCurrentView =  null;
+		createStateNWave(N, continuum, currentQESpace => {
+			// we've now got a qeSpace etc all set up
+			this.setState({N, continuum, currentQESpace});
+
+			// now create the view class instance as described by the space
+			const vClass = listOfViewClasses[currentQESpace.viewClassName];
+
+			// seems kinda funny doing these all here - but they should work for every view class
+			const currentView = new vClass('main view', this.canvas, currentQESpace);
+			currentView.completeView();
+
+			this.setState(currentView);
+
+			// kinda paranoid?
+			qe.theCurrentView = currentView;
+		});
+	}
+
+	// constructor runs twice, so do this once here
+	componentDidMount() {
+	}
+
+	render() {
+		const s = this.state;
+		let resDialog = null;  // what's this supposed to be?
+
+
+		debugger;
+		return (
+			<div className="SquishPanel">
+				{/*innerWindowWidth={s.innerWindowWidth}/>*/}
+				<SquishView setGLCanvas={canvas => this.setGLCanvas(canvas)} />
+				<ControlPanel
+
+				/>
+
+				{resDialog}
+			</div>
+		);
+
+	}
+}
+
+export default SquishPanel;
+
