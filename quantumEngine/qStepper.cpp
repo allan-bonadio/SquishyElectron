95f65369d9caf5183110aa6bc7f5bfb4f9336ce4
Allan Bonadio <bojnac@tactileint.org>
2021-07-05 16:26:51 -0700

    commented it all out; might not be used

--- quantumEngine/qStepper.cpp
+++ quantumEngine/qStepper.cpp
@@ -1,12 +1,14 @@
 // this manages the engine's stepping through time, subdividing
-int qStepper(void) {
-	const int many = 100;
+//int qStepper(void) {
+//	const int many = 100;
+//
+//    std::clock_t c_start = std::clock();
+//	for (int i = 0; i < many; i++) {
+//		theSpace->oneRk2Step();
+//	}
+//    std::clock_t c_end = std::clock();
+//    printf(" time for %d rk2 steps: %lf", many, (double)(c_end - c_start) / CLOCKS_PER_SEC);
+//	return many;
+//}
 
-    std::clock_t c_start = std::clock();
-	for (int i = 0; i < many; i++) {
-		theSpace->oneRk2Step();
-	}
-    std::clock_t c_end = std::clock();
-    printf(" time for %d rk2 steps: %lf", many, (double)(c_end - c_start) / CLOCKS_PER_SEC);
-	return many;
-}
+// someday
