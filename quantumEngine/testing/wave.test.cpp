d8ec8603843204ee84f70a5d213441ca9b96dc95
Allan Bonadio <bojnac@tactileint.org>
2021-07-05 16:39:39 -0700

    the wave test

---
+++ quantumEngine/testing/wave.test.cpp
@@ -0,0 +1,37 @@
+#include "../qSpace.h"
+#include "test.h"
+
+
+void handleEachPt(qCx psi, int ix) {
+	printf("I feel good!  %d: %lf %lf\n", ix, psi.re, psi.im);
+}
+
+void run_wave_tests(void) {
+	//  creating/
+	startNewSpace();
+	addSpaceDimension(3, contWELL, "p");
+
+	qWave  *wave = new qWave(theSpace);
+
+	// dumpWave
+	wave->dumpWave("unit test");
+
+	// forEachPoint() & forEachState
+	printf("forEachPoint() demo\n");
+	wave->forEachPoint(handleEachPt);
+
+	// fixBoundaries
+
+	// prune
+
+	// innerProduct
+
+	// normalize
+
+	// populate
+
+	// destroying
+	delete wave;
+	//delete theSpace;
+}
+
