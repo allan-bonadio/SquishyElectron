c49d09cceab692e261048bf1964a7f0bf1fa88c1
Allan Bonadio <bojnac@tactileint.org>
2021-07-11 17:13:24 -0700

    made dumpViewBuffer() its own function; added angles

--- quantumEngine/viewBuffer.cpp
+++ quantumEngine/viewBuffer.cpp
@@ -1,22 +1,26 @@
 #include "qSpace.h"
+#include <cmath>
 
 // prep wave & potential  data for GL.  For rows of floats in a big Float32Array,
 // will be fed directly into gl.
 float *viewBuffer;
 
+// for the JS side
 float *getViewBuffer(void) {
 	return (float *) viewBuffer;
 }
 
 // one row per vertex.
 // each row of 4 floats looks like this:
-//     real   imaginary    potential    [reservedForFuture]
+//     real   imaginary    potential    serial
 // Two vertices per datapoint: bottom then top, same data.
 // also converts to doubles from floats.
 float updateViewBuffer() {
 	int nPoints = theSpace->nPoints;
 	qReal highest = 0;
 	qReal tiny = 1e-8;
+
+	//theQWave->dumpWave("at start of updateViewBuffer()");
 
 	for (int pointNum = 0; pointNum < nPoints; pointNum++) {
 		float *twoRowPtr = viewBuffer + pointNum * 8;
@@ -41,39 +45,44 @@ float updateViewBuffer() {
 			highest = height;
 	}
 
-	if (true) {
+	// dump of viewbuffer.  Was good until I realized theWave itself was what to pay attention to.
+	if (false) {
 		printf("viewBuffer.cpp, as written to view buffer:\n");
-		for (int i = 0; i < nPoints*2; i++) {
-			printf("%6d   %6.3f  %6.3f  %6.3f  %6.3f\n",
-				i, viewBuffer[i*4], viewBuffer[i*4+1], viewBuffer[i*4+2], viewBuffer[i*4+3]);
-		}
+		dumpViewBuffer(nPoints);
 	}
 
-	if (true) {
-		printf("viewBuffer.cpp, resulting cx number:\n");
-		qReal avgProb = 1. / theSpace->nStates;
-		for (int i = 0; i < nPoints*2; i++) {
-			float re = viewBuffer[i*4];
-			float im = viewBuffer[i*4+1];
-			float prob = re * re + im * im;
-			if (i & 1) {
-				if (avgProb/8. > prob || prob > avgProb*8.) {
-					printf("bad prob in position %i: %6.3f + %6.3f => %6.3f\n",
-						i, viewBuffer[i*4], viewBuffer[i*4+1], prob);
-				}
-			}
-			else {
-				// should be about zero
-				if (-.000001 > prob || prob > .00001) {
-					printf("bad zero inner prod in position %i: %6.3f + %6.3f => %6.3f\n",
-						i, viewBuffer[i*4], viewBuffer[i*4+1], prob);
-				}
-			}
-			if (i != viewBuffer[i*4+3]) {
-				printf("bad serial in position %i: %f\n", i, viewBuffer[i*4+3]);
-			}
-		}
-	}
 	return highest;
 }
 
+
+// dump the view buffer just before it heads off to webgl.
+int dumpViewBuffer(int nPoints) {
+	qReal prevRe = viewBuffer[0];
+	qReal prevIm = viewBuffer[1];
+	printf("   ix  |    re      im     pot    serial  |   phase    magn\n");
+	for (int i = 0; i < nPoints*2; i++) {
+		qReal re = viewBuffer[i*4];
+		qReal im = viewBuffer[i*4+1];
+		if (i & 1) {
+			qReal dRe = re - prevRe;
+			qReal dIm = im - prevIm;
+			qReal phase = 0.;
+			qReal magn = 0.;
+			phase = atan2(im, re) * 180 / PI;
+			magn = im * im + re * re;
+			printf("%6d |  %6.3f  %6.3f  %6.3f  %6.3f  |  %6.3f  %6.3f\n",
+				i,
+				re, im, viewBuffer[i*4+2], viewBuffer[i*4+3],
+				phase, magn);
+
+			prevRe = re;
+			prevIm = im;
+		}
+		else {
+			printf("%6d |  %6.3f  %6.3f  %6.3f  %6.3f \n",
+				i,
+				re, im, viewBuffer[i*4+2], viewBuffer[i*4+3]);
+		}
+	}
+	return nPoints;
+}
