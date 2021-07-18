933a93d383e333393b73701e34df82f47fdae8b8
Allan Bonadio <orgmgr@tactileint.org>
2021-07-17 18:45:30 -0700

    other stuff ... too tired...

--- quantumEngine/testing/test.h
+++ quantumEngine/testing/test.h
@@ -1,3 +1,8 @@
+/*
+** blah blah -- like a source file for Squishy Electron
+** Copyright (C) 2021-2021 Tactile Interactive, all rights reserved
+*/
+
 extern void run_qCx_tests(void);
 extern void run_rk2_tests(void);
 extern void run_wave_tests(void);
