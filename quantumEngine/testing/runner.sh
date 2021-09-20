546ac423b401339979174a779b39cbf9966465b6
Allan Bonadio <bojnac@tactileint.org>
2021-09-19 23:56:08 -0700

    Now you can add --inspect cmdline flag

--- quantumEngine/testing/runner.sh
+++ quantumEngine/testing/runner.sh
@@ -3,24 +3,40 @@
 cd `dirname $0`
 cd ..
 
-echo "q test runner. Just run it from any directory, no args "
-echo "Read src to verify.  (must add new srcs to this file)"
+echo "q test runner. Just run it from any directory, no args needed"
+echo "Read src to verify.  (must add new test srcs to this file)"
+echo "add in argument of --inspect or --inspect-brk or any other node args"
+
 # source this to run MscriptN stuff:
 . /dvl/emscripten/emsdk-main/emsdk_env.sh
 
-# create a space-sep list of all the cpp files (almost all)
+# create a space-sep list of ALL the cpp files (almost all)
 allCpp=`cat building/allCpp.list`
+
+mt=" testing/main.test.cpp testing/qCx.test.cpp "
+et=" testing/rk2.test.cpp testing/vissFlicks.test.cpp "
+swt="testing/space.test.cpp  testing/wave.test.cpp "
+allTesters=" $mt $et $swt "
 
 # note that main.cpp is NOT included in the .cpp files; that's for web use only
 # and makes all the diff.  Update list of test srcs as needed.
 emcc -o quantumTest.js -sLLD_REPORT_UNDEFINED -g \
-	testing/main.test.cpp \
-	testing/qCx.test.cpp testing/rk2.test.cpp testing/wave.test.cpp \
-	testing/space.test.cpp testing/vissFlicks.test.cpp \
+	-g -sASSERTIONS=2 -sSAFE_HEAP=1 -sSTACK_OVERFLOW_CHECK=2 \
+	-sNO_DISABLE_EXCEPTION_CATCHING \
+	$allTesters \
 	$allCpp \
 	|| exit $?
 
+# added/removed mid sept: 	-fsanitize=undefined
+
+
 # now run the tests
 echo
+echo ========================================================================
 echo ========================================================== Results:
-node quantumTest.js
+echo ========================================================================
+
+
+node $* quantumTest.js
+
+
