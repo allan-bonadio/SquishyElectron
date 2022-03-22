f0f4d786d850575ad2bd1cce185d0de4aceec3b0
Allan Bonadio <bojnac@tactileint.org>
2022-03-21 22:14:59 -0700

    get rid of this xcode projectI tried to start, just to use the debugger.  I'll stick with lldb for now.

---
+++ quantumEngine/SquishyElectronTesting/SquishyElectronTesting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SquishyElectronTesting
+//
+//  Created by Allan Bonadio on 3/21/22.
+//  Copyright © 2022 Tactile Interactive. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
