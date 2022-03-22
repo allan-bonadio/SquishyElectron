1947423b814da776b448232ab7af5ef211c6d4af
Allan Bonadio <bojnac@tactileint.org>
2022-03-21 18:13:41 -0700

    added specific messages for each test call turning tests on and off

--- quantumEngine/spaceWave/qSpace.spec.cpp
+++ quantumEngine/spaceWave/qSpace.spec.cpp
@@ -39,6 +39,7 @@ TEST(qSpace, qSpace_BareGauntlet)
 }
 
 
+// just the constructor; it's not even fully created
 TEST(qSpace, qSpace_ConstructorGauntlet)
 {
 	printf("🧨 🧨  starting qSpace_ConstructorGauntlet\n");
@@ -55,37 +56,53 @@ TEST(qSpace, qSpace_ConstructorGauntlet)
 	delete space;
 }
 
+// this tests the whole shebang, as created from JS
+void tryOneSpaceLength(int N, int expectedSpectrumLength, int expectedFreeBufferLength) {
+	printf("🧨 🧨 starting tryOneSpaceLength(N=%d, sl=%d, fbl=%d)\n",
+		N, expectedSpectrumLength, expectedFreeBufferLength);
+	qSpace *space = makeFull1dSpace(N);
+	printf("🧨 🧨       created the space and all the buffers; freeBufferList=%p\n", space->freeBufferList);
+	int nPoints = space->nPoints;
+
+	STRCMP_EQUAL_TEXT(MAKEFULL1DSPACE_LABEL, space->label, "space label");
+
+	// these are for the dimension, of which there's only one
+	LONGS_EQUAL_TEXT(1, space->dimensions->start, "space start");
+	LONGS_EQUAL_TEXT(N+1, space->dimensions->end, "space end");
+	LONGS_EQUAL_TEXT(N, space->dimensions->N, "space N");
+	LONGS_EQUAL_TEXT(contENDLESS, space->dimensions->continuum, "space continuum");
+	LONGS_EQUAL_TEXT(N+2, space->dimensions->nPoints, "space nPoints");
+	LONGS_EQUAL_TEXT(N, space->dimensions->nStates, "space nStates");
+	STRCMP_EQUAL_TEXT("x", space->dimensions->label, "space label");
+	LONGS_EQUAL_TEXT(expectedSpectrumLength, space->dimensions->spectrumLength, "space spectrumLength");
+
+	LONGS_EQUAL_TEXT(expectedSpectrumLength, space->spectrumLength, "space spectrumLength");
+	LONGS_EQUAL_TEXT(expectedFreeBufferLength, space->freeBufferLength, "space freeBufferLength");
+
+	LONGS_EQUAL_TEXT(1, space->nDimensions, "space nDimensions");
+
+	// lets see if the buffers are all large enough
+	printf("🧨 🧨       lets see if the buffers are all large enough freeBufferList=%p\n", space->freeBufferList);
+	proveItsMine(laosWave, nPoints * sizeof(qCx));
+	proveItsMine(peruWave, nPoints * sizeof(qCx));
+	proveItsMine(theSpace->potential, nPoints * sizeof(double));
+	proveItsMine(theQViewBuffer->viewBuffer, nPoints * sizeof(float) * 8);
+
+	printf("🧨 🧨       we're done, deleting freeBufferList=%p\n", space->freeBufferList);
+	deleteTheSpace();
+	printf("🧨 🧨       tryOneSpaceLength() completed\n");
+}
+
 TEST(qSpace, qSpace_CompleteNewSpaceGauntlet)
 {
 	printf("🧨 🧨  starting qSpace_CompleteNewSpaceGauntlet\n");
-	qSpace *space = makeFull1dSpace(16);
-	//printf("🧨 🧨       created the space and all the buffers\n");
-	int nPoints = space->nPoints;
 
-	STRCMP_EQUAL(MAKEFULL1DSPACE_LABEL, space->label);
-
-	LONGS_EQUAL(1, space->dimensions->start);
-	LONGS_EQUAL(17, space->dimensions->end);
-	LONGS_EQUAL(16, space->dimensions->N);
-	LONGS_EQUAL(contENDLESS, space->dimensions->continuum);
-	LONGS_EQUAL(18, space->dimensions->nPoints);
-	LONGS_EQUAL(16, space->dimensions->nStates);
-	STRCMP_EQUAL("x", space->dimensions->label);
-
-	LONGS_EQUAL(16, space->dimensions->fourierSize);
-	LONGS_EQUAL(16, space->spectrumSize);
-	LONGS_EQUAL(18, space->freeBufferLength);
-
-	LONGS_EQUAL(18, space->nPoints);
-	LONGS_EQUAL(16, space->nStates);
-	LONGS_EQUAL(1, space->nDimensions);
-
-	// lets see if the buffers are all large enough
-	claimItsMine(laosWave, nPoints * sizeof(qCx));
-	claimItsMine(peruWave, nPoints * sizeof(qCx));
-	claimItsMine(theSpace->potential, nPoints * sizeof(double));
-	claimItsMine(theQViewBuffer->viewBuffer, nPoints * sizeof(float) * 8);
-
-	deleteTheSpace();
+	tryOneSpaceLength(4000, 4096, 4096);
+	tryOneSpaceLength(254, 256, 256);
+	tryOneSpaceLength(63, 64, 65);
+	tryOneSpaceLength(48, 64, 64);
+	tryOneSpaceLength(32, 32, 34);
+	tryOneSpaceLength(32, 32, 34);
+	tryOneSpaceLength(4, 4, 6);
 }
 
--- quantumEngine/spaceWave/qWave.spec.cpp
+++ quantumEngine/spaceWave/qWave.spec.cpp
@@ -1,7 +1,7 @@
 //#include <string>
 //#include "../spaceWave/qCx.h"
-#include "../spaceWave/qSpace.h"
-#include "../spaceWave/qWave.h"
+#include "./qSpace.h"
+#include "./qWave.h"
 #include "../testing/cppuMain.h"
 
 #include "CppUTest/TestHarness.h"
@@ -10,7 +10,9 @@
 //using namespace std;
 
 
-TEST_GROUP(qWave)
+/* ***************************************************************** Buffer */
+
+TEST_GROUP(qBuffer)
 {
 };
 
@@ -18,29 +20,32 @@ TEST_GROUP(qWave)
 // in the middle, check several values that should be right
 // and make sure we own the bytes we think we do.
 static void testABufferLength(int length, qCx *useThisBuffer = NULL) {
-	printf(": testing %d long buffer with x%p\n", length, useThisBuffer);
+	// uncomment this if you want to see which one failed
+	printf("🧨: testing %d point buffer with x%p\n", length, useThisBuffer);
 
 	qBuffer *qBuf = new qBuffer();
-	LONGS_EQUAL('qBuf', qBuf->magic);
-	POINTERS_EQUAL(NULL, qBuf->wave);
+	LONGS_EQUAL_TEXT('qBuf', qBuf->magic, "qbuffer magic");
+	POINTERS_EQUAL_TEXT(NULL, qBuf->wave, "qbuffer wave null");
 
 	qBuf->initBuffer(length, useThisBuffer);
-	LONGS_EQUAL(length, qBuf->nPoints);
+	LONGS_EQUAL_TEXT(length, qBuf->nPoints, "qbuffer nPoints");
 
 	if (useThisBuffer){
-		CHECK(! qBuf->dynamicallyAllocated);
-	}else{
-		CHECK(qBuf->dynamicallyAllocated);
-}
-	CHECK(qBuf->wave);
+		CHECK_TEXT(! qBuf->dynamicallyAllocated, "qbuffer !dynamicallyAllocated");
+	}
+	else {
+		CHECK_TEXT(qBuf->dynamicallyAllocated, "qbuffer dynamicallyAllocated");
+	}
 
-	claimItsMine(qBuf->wave, length * sizeof(qCx));
+	CHECK_TEXT(qBuf->wave, "qbuffer wave nonnull");
+
+	proveItsMine(qBuf->wave, length * sizeof(qCx));
 
 	delete qBuf;
 }
 
 
-TEST(qWave, BufferConstructDestruct)
+TEST(qBuffer, BufferConstructDestruct)
 {
 	testABufferLength(1);
 	testABufferLength(4);
@@ -54,65 +59,121 @@ TEST(qWave, BufferConstructDestruct)
 	testABufferLength(3233);
 }
 
-TEST(qWave, BufferUseThisBuffer)
+TEST(qBuffer, BufferUseThisBufferTwoAtOnce)
 {
-	qCx *buf;
+	// make sure there isn't anything shared between them.
+	qCx *buf555 = (qCx *) malloc(555 * sizeof(qCx));
+	qCx *buf8 = (qCx *) malloc(8 * sizeof(qCx));
 
-	buf = (qCx *) malloc(8 * sizeof(qCx));
-	testABufferLength(8, buf);
-	free(buf);
+	testABufferLength(8, buf8);
+	testABufferLength(555, buf555);
 
-	buf = (qCx *) malloc(555 * sizeof(qCx));
-	testABufferLength(555, buf);
-	free(buf);
+	free(buf555);
+	free(buf8);
 }
 
-TEST(qWave, allocateFree)
+TEST(qBuffer, BufferAllocateFreeTwoAtOnce)
 {
-	qCx *buf;
+	// make sure there isn't anything shared between them.
+	qCx *buf27 = allocateWave(27);
+	qCx *buf999 = allocateWave(999);
 
-	buf = allocateWave(27);
-	testABufferLength(27, buf);
-	freeWave(buf);
+	testABufferLength(999, buf999);
+	testABufferLength(27, buf27);
 
-	buf = allocateWave(999);
-	testABufferLength(999, buf);
-	freeWave(buf);
+	freeWave(buf27);
+	freeWave(buf999);
 }
 
+
+/* ***************************************************************** Wave */
+
+TEST_GROUP(qWave)
+{
+};
+
+
+static void tryOutWave(int N) {
+	printf("🧨 🧨  starting tryOutWave(N=%d)\n", N);
+	qSpace *space = makeFull1dSpace(N);
+	printf("🚀 🚀      tryOutWave: freeBufferList: x%p\n", space->freeBufferList);
+
+//	qWave *qWav = new qWave(space);
+
+//	LONGS_EQUAL_TEXT('qWav', qWav->magic, "qwave magic");
+//	CHECK_TEXT(qWav->wave, "qwave wave");
+//	CHECK_TEXT(qWav->dynamicallyAllocated, "qwave dynamicallyAllocated");
+//
+//	LONGS_EQUAL_TEXT(1, qWav->start, "qwave start");
+//	LONGS_EQUAL_TEXT(N+1, qWav->end, "qwave end");
+//	LONGS_EQUAL_TEXT(N+2, qWav->nPoints, "qwave nPoints");
+
+//	proveItsMine(qWav->wave, qWav->nPoints * sizeof(qCx));
+
+//	delete qWav;
+	delete space;
+}
 
 TEST(qWave, WaveConstructDestruct)
 {
-//	LONGS_EQUAL(5, qBuf->nPoints);
-//	LONGS_EQUAL(5, qBuf->nPoints);
+//	tryOutWave(4);
+//	tryOutWave(31);
+	tryOutWave(32);
+	tryOutWave(33);
+	tryOutWave(33);
+	tryOutWave(600);
+	tryOutWave(1024);
 }
 
 
-TEST(qWave, SpectrumConstructDestruct)
+
+/* ***************************************************************** Spectrum */
+
+TEST_GROUP(qSpectrum)
 {
-//	LONGS_EQUAL(5, qBuf->nPoints);
-//	LONGS_EQUAL(5, qBuf->nPoints);
+};
+
+static void tryOutSpectrum(int N, int expectedSpLength, int expectedFBLength) {
+	printf("🧨 🧨  starting tryOutSpectrum(N=%d, sl=%d, fbl=%d)\n",
+		N, expectedSpLength, expectedFBLength);
+	qSpace *space = makeFull1dSpace(N);
+	printf("🧨 🧨    made it this far, %s:%d\n", __FILE__, __LINE__);
+	qSpectrum *qSpe = new qSpectrum(space);
+	printf("🧨 🧨    made it this far, %s:%d\n", __FILE__, __LINE__);
+
+	// whatsi sposed to be?  Find next powerof 2.  Try to do it differently
+	// from the software; not nec elegant
+	int po2 = 1;
+	for (int j = 1; j < 4096; j *= 2) {
+		if (j & (N-1))
+			po2 = j * 2;
+	}
+	printf("🧨 🧨    the po2 is %d, from N=%d\n", po2, N);
+
+	LONGS_EQUAL_TEXT('qSpe', qSpe->magic, "qspectrum magic");
+	CHECK_TEXT(qSpe->wave, "qspectrum wave");
+	CHECK_TEXT(qSpe->dynamicallyAllocated, "qspectrum dynamicallyAllocated");
+
+	// nPoints == spectrumLength
+	LONGS_EQUAL_TEXT(po2, qSpe->nPoints, "qspectrum nPoints");
+	LONGS_EQUAL_TEXT(expectedFBLength, space->freeBufferLength, "qspectrum freeBufferLength");
+
+	//proveItsMine(qSpe->wave, qSpe->nPoints * sizeof(qCx));
+
+	delete qSpe;
+	delete space;
 }
 
+IGNORE_TEST(qSpectrum, qSpectrumConstructDestruct)
+{
+	//tryOutSpectrum(4, 4, 6);
+	tryOutSpectrum(31, 32, 33);
+	//tryOutSpectrum(32, 32, 34);
+	//tryOutSpectrum(33, 64, 64);
+	tryOutSpectrum(101, 128, 128);
+	tryOutSpectrum(600, 1024, 1026);
+	tryOutSpectrum(2048, 2048, 2050);
+}
 
-//TEST(qWave, BufferConstructDestruct)
-//{
-//}
-//
-//
-//TEST(qWave, BufferConstructDestruct)
-//{
-//}
-//
-//
-//TEST(qWave, BufferConstructDestruct)
-//{
-//}
-//
-//
-//TEST(qWave, BufferConstructDestruct)
-//{
-//}
-//
 
 
