b3d6fbae6e9261b0bc4c3f7dcc83e2a572862a09
Allan Bonadio <bojnac@tactileint.org>
2022-03-27 00:48:01 -0700

    the squish.h file itself

---
+++ quantumEngine/squish.h
@@ -0,0 +1,16 @@
+/*
+** squish.h -- common defines for the C++ part of squishyelectron
+** Copyright (C) 2021-2022 Tactile Interactive, all rights reserved
+*/
+
+//include this in almost all C++ sources
+
+#include <cmath>
+#include <stdio.h>
+#include <stdexcept>
+
+//?#include <string>
+//?
+
+#include "spaceWave/qCx.h"
+
