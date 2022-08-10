3c824798f7d036b15d9f7f461ffc5b3b1c43258f
Allan Bonadio <bojnac@tactileint.org>
2022-08-10 05:26:48 -0700

    cleanup; took out list mode... more work to do

--- src/widgets/TextNSlider.js
+++ src/widgets/TextNSlider.js
@@ -12,6 +12,7 @@ function setPT() {
 		label: PropTypes.string,
 		style: PropTypes.object,
 
+		// should be a number, or a string of a number
 		value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 		min: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 		max: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
@@ -26,7 +27,7 @@ function setPT() {
 	TextNSlider.defaultProps = {
 		className: '',
 		style: {},
-		step: 1,
+		step: 'any',  // means no step, for if you use list
 
 		handleChange: (ix, power) => {},
 	};
@@ -52,16 +53,42 @@ function TextNSlider(props) {
 		p.handleChange(val)
 	}
 
+	let controls;
+	if (p.list) {
+		// a list.  Must use integers as pseudovalues for the list items.
+		// You're responsible for for making the list and dreaming up an ID.
+		// Um, no, I'll have to finsih this later.
+		// controls = <>
+		// 	<input type='number' placeholder={p.label}
+		// 			value={p.value} min={p.min} max={p.max}
+		// 			list={p.list} step='any'
+		// 			size='7'
+		// 			onChange={handleChange} />
+		// 	<input type='range'
+		// 			value={p.value} min={p.min} max={p.max} step={p.step}
+		// 			list={p.list}
+		// 			onChange={handleChange} />
+		// </>;
+	}
+	else {
+		// to tweak the display in the text box, do it in the render.  Then when retrieving the number, convert it back.
+		controls = <>
+			<input type='number' placeholder={p.label || ''}
+					value={p.value} min={p.min} max={p.max}
+					step={p.step}
+					size='7'
+					onChange={handleText} />
+			<input type='range'
+					value={p.value} min={p.min} max={p.max}
+					step={p.step}
+					onChange={handleSlider} />
+		</>;
+	}
+
 	const label = p.label ? <span>{p.label}</span> : '';
 	return <div className={`TextNSlider {p.className}`} style={p.style}>
 		{label}
-		<input type='number' placeholder={label}
-				value={p.value} min={p.min} max={p.max} step={p.step}
-				size='5'
-				onChange={handleText} />
-		<input type='range'
-				value={p.value} min={p.min} max={p.max} step={p.step}
-				onChange={handleSlider} />
+		{controls}
 	</div>;
 }
 
