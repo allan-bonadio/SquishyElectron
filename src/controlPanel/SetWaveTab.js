173b5c43b0cbbbb50d6dfd6b36ab30f9b048c93f
Allan Bonadio <bojnac@tactileint.org>
2022-08-22 23:11:40 -0700

    finally get rid of the recipe

--- src/controlPanel/SetWaveTab.js
+++ src/controlPanel/SetWaveTab.js
@@ -11,13 +11,13 @@ import {scaleLinear} from 'd3-scale';
 
 import MiniGraph from './MiniGraph';
 import eSpace from '../engine/eSpace';
-import eWave from '../engine/eWave';
-import eCx from '../engine/eCx';
-import cxToRgb from '../view/cxToRgb';
+// import eWave from '../engine/eWave';
+// import eCx from '../engine/eCx';
+// import cxToRgb from '../view/cxToRgb';
 import TextNSlider from '../widgets/TextNSlider';
 import {storeASetting, alternateMinMaxs} from '../utils/storeSettings';
 
-let debugWaveTab = false;
+//let debugWaveTab = false;
 
 function setPT() {
 	// variables from on high, and the funcs needed to change them
@@ -50,58 +50,61 @@ class SetWaveTab extends React.Component {
 	// Returns a <g element enclosing the juicy stuff
 	recipe =
 	(miniSpace, waveParams) => {
+		// temporarily disabled
+		return '';
+
 		//const p = this.props;
-		const {start, end, N, nPoints} = miniSpace.startEnd2;
-
-		// keep these buffers around for reuse - a bit faster
-		if (! this.qewave || this.elements.length != nPoints) {
-			this.qewave = new eWave(miniSpace);
-			this.elements = new Array(N);
-			this.magns = new Float64Array(nPoints/2);
-		}
-
-		// generate the values
-		this.qewave.setFamiliarWave(waveParams);
-		const wave = this.qewave.wave;
-
-		// find yMin and yMax
-		let magn;
-		let maxY = 4 / N;
-		for (let ix = start; ix < end; ix += 2) {
-			magn = this.magns[ix/2] = wave[ix] ** 2 + wave[ix+1] ** 2;
-			maxY = Math.max(maxY, magn);
-
-			if (debugWaveTab) console.log(
-				`miniGraph magn${ix} = '${magn}' `);
-		}
-
-		// a bit tighter so we can fit in all the bar widths
-		const barWidth = this.miniWidth / N;
-		const halfWidth = barWidth / 2;
-		const xScale = scaleLinear()
-			.range([halfWidth, this.miniWidth - halfWidth])
-			.domain([start/2, end/2-1]);
-		this.yScale.domain([0, maxY]);  // upside down!
-
-		//  generate the <path elements
-		let start1 = start / 2;
-		for (let ix = start; ix < end; ix += 2) {
-			let ix1 = ix/2;
-			let x = xScale(ix1);
-			let color = cxToRgb(eCx(wave[ix], wave[ix+1]));
-			magn = this.magns[ix1];
-			magn = this.yScale(magn);
-
-			if (debugWaveTab) console.log(
-				`miniGraph point [${x}] = ${magn}   color = ${color}`);
-
-			this.elements[ix1 - start1] = <path  d={`M${x},0V${magn}`}
-				stroke={color} strokeWidth={barWidth} fill='none' key={ix}/>
-		}
-
-		return <g className='linePaths' >
-				{this.elements}
-			</g>;
+// 		const {start, end, N, nPoints} = miniSpace.startEnd2;
+//
+// 		// keep these buffers around for reuse - a bit faster
+// 		if (! this.qewave || this.elements.length != nPoints) {
+// 			this.qewave = new eWave(miniSpace);
+// 			this.elements = new Array(N);
+// 			this.magns = new Float64Array(nPoints/2);
+// 		}
+//
+// 		// generate the values
+// 		this.qewave.setFamiliarWave(waveParams);
+// 		const wave = this.qewave.wave;
+//
+// 		// find yMin and yMax
+// 		let magn;
+// 		let maxY = 4 / N;
+// 		for (let ix = start; ix < end; ix += 2) {
+// 			magn = this.magns[ix/2] = wave[ix] ** 2 + wave[ix+1] ** 2;
+// 			maxY = Math.max(maxY, magn);
+//
+// 			if (debugWaveTab) console.log(
+// 				`miniGraph magn${ix} = '${magn}' `);
+// 		}
+//
+// 		// a bit tighter so we can fit in all the bar widths
+// 		const barWidth = this.miniWidth / N;
+// 		const halfWidth = barWidth / 2;
+// 		const xScale = scaleLinear()
+// 			.range([halfWidth, this.miniWidth - halfWidth])
+// 			.domain([start/2, end/2-1]);
+// 		this.yScale.domain([0, maxY]);  // upside down!
+//
+// 		//  generate the <path elements
+// 		let start1 = start / 2;
+// 		for (let ix = start; ix < end; ix += 2) {
+// 			let ix1 = ix/2;
+// 			let x = xScale(ix1);
+// 			let color = cxToRgb(eCx(wave[ix], wave[ix+1]));
+// 			magn = this.magns[ix1];
+// 			magn = this.yScale(magn);
+//
+// 			if (debugWaveTab) console.log(
+// 				`miniGraph point [${x}] = ${magn}   color = ${color}`);
+//
+// 			this.elements[ix1 - start1] = <path  d={`M${x},0V${magn}`}
+// 				stroke={color} strokeWidth={barWidth} fill='none' key={ix}/>
+// 		}
+//
+// 		return <g className='linePaths' >
+// 				{this.elements}
+// 			</g>;
 	}
 
 	setBreed = breed => {
